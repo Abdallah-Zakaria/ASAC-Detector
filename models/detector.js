185bb587ed25be13c47fb6a97361a53935991bd6
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2021-06-26 02:40:47 +0300

    Add to the logs

--- models/detector.js
+++ models/detector.js
@@ -1,11 +1,30 @@
-'use strict';
+"use strict";
 
-module.exports = (client)=>{
-  client.on("message", (message) => {
+const Discord = require("discord.js");
+
+module.exports = (client) => {
+  client.on("message", async (message) => {
+    console.log(message)
     const { content } = message;
     const arabic = /[\u0600-\u06FF]/;
     if (arabic.test(content)) {
-      message.channel.send("🚨🛑 YOU ARE ONLY ALLOWED TO SPEAK IN ENGLISH HERE! 🛑🚨");
+      const channelName = message.channel.name;
+      const userName = message.author.username;
+      message.channel.send(
+        "🚨🛑 YOU ARE ONLY ALLOWED TO SPEAK IN ENGLISH HERE! 🛑🚨"
+      );
+      const channel = await client.channels.fetch("858123323653357598");
+      const embed = new Discord.MessageEmbed()
+        .setDescription(
+        `
+        We found ${userName} that assign in ${channelName} writing in Arabic.
+        ${content}
+        `
+        )
+        .setTitle("Writing Arabic")
+        .setFooter("Warning")
+        .setColor("#b006c6");
+      channel.send(embed);
     }
   });
-}
+};
